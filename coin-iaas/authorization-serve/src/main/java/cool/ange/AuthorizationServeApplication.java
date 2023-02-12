9eb7c2289405ea3ca4720ba2c5555fd233945c67
ange <2899851554@qq.com>
2023-02-12 20:21:12 +0800

    authorization授权中心初步搭建

---
+++ coin-iaas/authorization-serve/src/main/java/cool/ange/AuthorizationServeApplication.java
@@ -0,0 +1,21 @@
+package cool.ange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author: ange
+ * @package: cool.ange
+ * @className: AuthorizationServeApplication
+ * @creationTime: 2023-02-12 19:28
+ * @Version: v1.0
+ * @description: todo
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthorizationServeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServeApplication.class,args);
+    }
+}
