55f749b7f5e9b0b1855a03a35d80ca59f83a9cda
ange <2899851554@qq.com>
2023-03-22 19:39:33 +0800

    优化登录

--- coin-admin/admin-service/src/main/java/cool/ange/controller/SysLoginController.java
+++ coin-admin/admin-service/src/main/java/cool/ange/controller/SysLoginController.java
@@ -1,8 +1,11 @@
 package cool.ange.controller;
 
 import cool.ange.model.LoginResult;
+import cool.ange.model.ResponseResult;
 import cool.ange.service.SysLoginService;
 import io.swagger.annotations.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,8 +40,17 @@ public class SysLoginController {
             @ApiImplicitParam(name = "username", value = "登录账户", required = true, paramType = "query", dataType = "String"),
             @ApiImplicitParam(name = "username", value = "登录密码", required = true, paramType = "query", dataType = "String")
     })
-    public LoginResult login(@ApiParam(name = "username", value = "登录账户", required = true) String username,
-                             @ApiParam(name = "password", value = "登录密码", required = true) String password) {
-        return sysLoginService.login(username, password);
+    public ResponseResult<LoginResult> login(@ApiParam(name = "username", value = "登录账户", required = true) String username,
+                                @ApiParam(name = "password", value = "登录密码", required = true) String password) {
+        return ResponseResult.success("success",sysLoginService.login(username, password));
+    }
+
+    /**
+     * 刷新token，功能展示不确定是否需要，先写一个假接口
+     * @return ResponseResult<String>
+     */
+    @GetMapping("/refreshToken")
+    public ResponseResult<String> GetRefreshToken(){
+        return ResponseResult.success("success","功能暂时不确定是否需要，先写一个假接口");
     }
 }
