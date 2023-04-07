81a5c371ee743a59dc54ea8f7e8b0347aa285835
ange <2899851554@qq.com>
2023-04-07 16:03:31 +0800

    登录优化

--- coin-admin/admin-service/src/main/java/cool/ange/controller/SysLoginController.java
+++ coin-admin/admin-service/src/main/java/cool/ange/controller/SysLoginController.java
@@ -4,10 +4,12 @@ import cool.ange.model.LoginResult;
 import cool.ange.model.ResponseResult;
 import cool.ange.service.SysLoginService;
 import io.swagger.annotations.*;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @author: ange
@@ -29,28 +31,25 @@ public class SysLoginController {
 
     /**
      * 后台人员登录接口
-     *
-     * @param username 登录账户
-     * @param password 登录密码
      * @return LoginResult 登录的结果
      */
     @PostMapping("/login")
     @ApiOperation(value = "后台登录接口", httpMethod = "POST", notes = "后台管理人员登录系统接口，需要传递登录账户、密码参数。返回一个LoginResult实体类")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "username", value = "登录账户", required = true, paramType = "query", dataType = "String"),
-            @ApiImplicitParam(name = "username", value = "登录密码", required = true, paramType = "query", dataType = "String")
+            @ApiImplicitParam(name = "password", value = "登录密码", required = true, paramType = "query", dataType = "String")
     })
-    public ResponseResult<LoginResult> login(@ApiParam(name = "username", value = "登录账户", required = true) String username,
-                                @ApiParam(name = "password", value = "登录密码", required = true) String password) {
-        return ResponseResult.success("success",sysLoginService.login(username, password));
+    public ResponseResult<LoginResult> login(@RequestBody Map<String, String> params) {
+        return ResponseResult.success("success", sysLoginService.login(params.get("username"), params.get("password")));
     }
 
     /**
      * 刷新token，功能展示不确定是否需要，先写一个假接口
+     *
      * @return ResponseResult<String>
      */
     @GetMapping("/refreshToken")
-    public ResponseResult<String> GetRefreshToken(){
-        return ResponseResult.success("success","功能暂时不确定是否需要，先写一个假接口");
+    public ResponseResult<String> getRefreshToken() {
+        return ResponseResult.success("success", "功能暂时不确定是否需要，先写一个假接口");
     }
 }
