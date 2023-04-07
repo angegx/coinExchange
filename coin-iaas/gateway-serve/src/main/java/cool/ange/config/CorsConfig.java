8443f8e05c37f861ebc4201d977903adbfaa47c3
ange <2899851554@qq.com>
2023-04-07 15:53:38 +0800

    解决跨域问题

---
+++ coin-iaas/gateway-serve/src/main/java/cool/ange/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package cool.ange.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author: ange
+ * @package: cool.ange.config
+ * @className: CorsConfig
+ * @creationTime: 2023-03-24 14:53
+ * @Version: v1.0
+ * @description: 解决前端跨域问题
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(urlBasedCorsConfigurationSource) ;
+    }
+}
