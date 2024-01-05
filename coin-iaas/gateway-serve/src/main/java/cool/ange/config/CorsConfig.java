1fa9ad6e82f2da4610763638c5ad07e8721c6db4
ange <angegx.com@gmail.com>
2024-01-05 17:57:54 +0800

    2024-01-05 17：55 对gateway网关服务模块中的跨域配置类进行注释

--- coin-iaas/gateway-serve/src/main/java/cool/ange/config/CorsConfig.java
+++ coin-iaas/gateway-serve/src/main/java/cool/ange/config/CorsConfig.java
@@ -12,19 +12,32 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
  * @className: CorsConfig
  * @creationTime: 2023-03-24 14:53
  * @Version: v1.0
- * @description: 解决前端跨域问题
+ * @description: 解决前端跨域问题，再后端配置跨问题
  */
 @Configuration
 public class CorsConfig {
+
+    /**
+     * 网关配置跨域
+     *
+     * @return CorsWebFilter 返回跨域的配置bean
+     */
     @Bean
-    public CorsWebFilter corsWebFilter(){
+    public CorsWebFilter corsWebFilter() {
+        //创建 CorsConfiguration 跨域配置信息对象
         CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //配置允许跨域访问的请求头信息， *表示所有的的都允许
         corsConfiguration.addAllowedHeader("*");
+        //配置允许跨域访问的url，*表示所有的url都允许
         corsConfiguration.addAllowedOrigin("*");
+        //配置允许跨域访问的请求方式，*表示有的请求方式都允许
         corsConfiguration.addAllowedMethod("*");
 
+        //创建 UrlBasedCorsConfigurationSource 基于Url的Cors配置源对象
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
-        return new CorsWebFilter(urlBasedCorsConfigurationSource) ;
+        //记录上面配置的跨域设置
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+        //创建 CorsWebFilter 对象，并将配置的跨域设置，作为参数设置到CorsWebFilter中
+        return new CorsWebFilter(urlBasedCorsConfigurationSource);
     }
 }
